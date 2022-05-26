441bcd7f6d23944f76bd4c5402e4074f87d7cbc2
Alan <naalagappan@users.noreply.github.com>
2022-05-26 18:02:59 +0530

    Add files via upload

---
+++ mastering-spring-boot/mastering-spring-boot/src/test/java/com/dineshonjava/masteringspringboot/MasteringSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineshonjava.masteringspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
